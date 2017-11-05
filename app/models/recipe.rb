4c610c2f5845069879b68eea8a47155d13085c69
Naeem Haider <naeemh93@gmail.com>
2017-11-05 21:06:34 +0500

    index page Styling and setup

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe <ApplicationRecord
   validates :name, presence:true
   validates :description ,presence: true , length:{minimum: 5 ,maximum: 5000}
-  belongs_to :chef ,presence:true
+  belongs_to :chef
   validates :chef_id ,presence:true
 end
