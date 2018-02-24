4fb6f1ea8099dff6201700d89145faadf5222710
naeemh93 <naeemh93@gmail.com>
2018-02-24 18:11:06 +0500

    email validation

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,5 +3,6 @@ class Chef <ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email ,presence:true ,length: {maximum: 500},
             format:{with: VALID_EMAIL_REGEX }
+  before_save { self.email = email.downcase }
   has_many :recipes
 end
